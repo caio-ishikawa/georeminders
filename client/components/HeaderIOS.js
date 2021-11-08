44afbf1c1b0da1a230150e4bc76fe36eee03b136
caio ishikawa <caioarbishikawa@gmail.com>
2021-11-08 16:38:09 +0000

    hidden api key

--- client/components/HeaderIOS.js
+++ client/components/HeaderIOS.js
@@ -3,11 +3,16 @@ import React, { Component, useState } from 'react';
 import { Header, Card } from 'react-native-elements';
 import SearchBar from 'react-native-elements/dist/searchbar/SearchBar-ios';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-
+import { apiKey } from '../secrets';
 
 
 const HeaderIOS = () => {
     const [search, setSearch] = useState();
+
+    const getCoords = (data) => {
+        let address = data.structured_formatting.secondary_text;
+        console.log(address);
+    };
 
     return(
         <GooglePlacesAutocomplete
@@ -26,11 +31,9 @@ const HeaderIOS = () => {
                 borderRadius: "20%"
             },
         }}
-        onPress={(data, details = null) => {
-            console.log(data, details);
-        }}
+        onPress={(data) => getCoords(data)}
         query={{
-            key: "AIzaSyAD63LGkmRT2kXTSHyaKBO5x6qIP9Sjb4U",
+            key: apiKey,
             language: "en"
         }}
         />
