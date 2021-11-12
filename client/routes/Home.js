cb1ba75704594576cc584a32e9f0437b9901ec29
caio ishikawa <caioarbishikawa@gmail.com>
2021-11-12 18:55:36 +0000

    logo change

--- client/routes/Home.js
+++ client/routes/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import {View, Text, Pressable, Image, StyleSheet, TouchableOpacity, Button} from 'react-native';
-import logo4 from '../assets/logo4.png';
+import reminderlogo from '../assets/reminderlogo.png';
 import {LinearGradient} from 'expo-linear-gradient';
 import * as SecureStore from 'expo-secure-store';
 
@@ -10,16 +10,16 @@ const Home = ({ navigation }) => {
     // Navigate to main page if username is stored in memory //
     useEffect(async() => {
         let storedUser = await SecureStore.getItemAsync('username');
-        if (storedUser) {
-            navigation.navigate("Main");
-        }
+        // if (storedUser) {
+        //     navigation.navigate("Main");
+        // }
     },[]);
 
     return(
         <View style={styles.cont}>
-            <Image style={styles.logo} source={logo4}/>
+            <Image style={styles.logo} source={reminderlogo}/>
             <LinearGradient start={[0,1]} end={[1, 0]} colors={[ '#3f5efb', "#a751ac", "#fc466b", "#ff8d0a" ]} style={styles.linear}>
-                <Button onPress={() => navigation.navigate("Register")} color="white" title="Register" style={styles.login}/>
+                <Button onPress={() => navigation.navigate("Register")} color="white" title="Get started" style={styles.login}/>
             </LinearGradient>
             <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Login")}>
                 <Text style={styles.login}>Log in</Text>
@@ -33,9 +33,9 @@ const styles = StyleSheet.create({
     logo:{
         alignSelf: "center",
         resizeMode: 'contain',
-        width: 300,
-        height:300,
-        marginTop: "10%",
+        width: 500,
+        height:400,
+        marginTop: "20%"
     },
     input: {
         borderWidth: 1,
@@ -59,8 +59,10 @@ const styles = StyleSheet.create({
         width: "60%",
         alignSelf: "center",
         padding: 5,
-        borderRadius: 5,
-        marginTop: "60%" 
+        borderRadius: 25,
+        borderColor: "white",
+        overflow: "hidden",
+        marginTop: "10%"
     },
     regPrompt:{
         color: "grey",
@@ -71,19 +73,28 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: "white",
         justifyContent: "center",
-        borderRadius: 5,
+        borderRadius: 25,
         width: "60%",
         alignSelf: "center",
         height:"5.5%",
         marginTop: "5%",
-        backgroundColor: "#FFF9EC"
     },
     login: {
-        color: "orange",
+        color: "white",
         fontWeight: "600",
         alignSelf: "center",
         fontSize:17.5 
-
+    },
+    welcome: {
+        color: "white",
+        fontSize: 35,
+        alignSelf: "center",
+        marginTop: "30%"
+    },
+    logoWelcome: {
+        color: "orange",
+        fontSize: 35,
+        alignSelf: "center"
     }
 });
 
